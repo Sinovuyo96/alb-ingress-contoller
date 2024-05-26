0318fd474959e4dfdb2b8a767e59078c9711eb85
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-26 20:55:35 +0200

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "test_lambda" {
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
   function_name = "test-s3-put-lambda-func"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
