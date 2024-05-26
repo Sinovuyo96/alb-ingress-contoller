901265c27694c2b18ddeb0cef27896b08f2cc250
Sino <51360698+Sinovuyo96@users.noreply.github.com>
2024-05-26 19:09:00 +0200

    Update lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -8,13 +8,13 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "test-s3-put-lambda-func"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler       = "lambda_handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "nodejs18.x"
+  runtime = "python.3.9"
 
   environment {
     variables = {
